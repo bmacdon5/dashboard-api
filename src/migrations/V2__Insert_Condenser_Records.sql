55e4f3dff1c6e0f64d45c5b525df488fb3c1a497
Thomas Fowler <thomas.fowler@flatironschool.com>
2021-11-08 01:12:32 -0600

    Updated migrations.

---
+++ src/migrations/V2__Insert_Condenser_Records.sql
@@ -0,0 +1,6 @@
+INSERT INTO condenser (condenserid, name, image)
+    VALUES ( '190821a1-1a3a-47da-9aab-e6edbba881ca', 'Artoo', 'condenser1.png'),
+           ( '2f9f1ecf-5fb2-4642-8b9f-2a9c8f02e442', 'Ben', 'condenser2.png'),
+           ('ea443bf8-3db2-41d8-8a1a-a691568e1e93', 'Chewie', 'condenser3.png'),
+           ('00f7add6-e164-4044-a708-602b83ce3863', 'Han', 'condenser4.png'),
+           ('dfbd9e3e-9d84-4c4d-b63b-cba332631bd5', 'Lando', 'condenser5.png');
