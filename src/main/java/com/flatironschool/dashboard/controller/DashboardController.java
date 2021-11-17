f194fd93ac490d38edccf6800a4803eb6535bf6a
bmacdon5 <93538774+bmacdon5@users.noreply.github.com>
2021-11-17 12:13:34 -0800

    Update DashboardController.java

--- src/main/java/com/flatironschool/dashboard/controller/DashboardController.java
+++ src/main/java/com/flatironschool/dashboard/controller/DashboardController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController()
-@RequestMapping("/api/v1")
+@RequestMapping("/")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DashboardController {
     @Autowired
